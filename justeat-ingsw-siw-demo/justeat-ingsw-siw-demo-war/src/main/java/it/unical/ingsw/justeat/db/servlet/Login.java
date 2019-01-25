d2bb4f2cd31496a0e350eb3f61c4f2e908c5735a
Riccardo Mallamaci <rickym6296@gmail.com>
2019-01-25 16:53:41 +0100

    ooo

---
+++ justeat-ingsw-siw-demo/justeat-ingsw-siw-demo-war/src/main/java/it/unical/ingsw/justeat/db/servlet/Login.java
@@ -0,0 +1,30 @@
+package it.unical.ingsw.justeat.db.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet(name="Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		
+		System.out.println("ciao");
+			HttpSession session= request.getSession();
+			session.setAttribute("username", uname);
+			response.sendRedirect("ciao.jsp");
+		
+	}
+
+	
+
+}
